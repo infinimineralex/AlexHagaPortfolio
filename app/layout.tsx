37f5cbd45d8efc52cc3228a7607eace6b7785f22
Alexander Haga <ivoRobotnik36@gmail.com>
2025-03-20 19:57:53 -0700

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from 'next/font/google';
 import './globals.css';
 
@@ -12,7 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>{children}
+            <SpeedInsights />
+            </body>
         </html>
     );
 }
